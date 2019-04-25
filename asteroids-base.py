97233afc80e60e2297ab23b48cc3670b75f2734b
Fesilds <felipejm2@al.insper.edu.br>
2019-04-25 15:29:55 -0300

    Parte 1 completa do codigo

--- asteroids-base.py
+++ asteroids-base.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-#testandooooooooooooooooo
 # Importando as bibliotecas necessárias.
 import pygame
 from os import path
@@ -20,6 +19,28 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
+#Classe Jogador que representa a nave
+class Player(pygame.sprite.Sprite):
+    #Construtor da classe
+    def __init__(self):
+        
+        #Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+        
+        #Carregando a imagem de fundo.
+        player_img = pygame.image.load(path. join(img_dir, "playerShip1_orange.png"))
+        self.image = player_img
+        
+        #Diminuindo o tamanho da imagem.
+        self.image = pygame.transform.scale(player_img, (50, 38))
+        
+        #Deixando transparente.
+        self.rect = self.image .get_rect()
+        
+        #Centralizar embaixo da tela
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        
 # Inicialização do Pygame.
 pygame.init()
 pygame.mixer.init()
@@ -36,6 +57,13 @@ clock = pygame.time.Clock()
 # Carrega o fundo do jogo
 background = pygame.image.load(path.join(img_dir, 'starfield.png')).convert()
 background_rect = background.get_rect()
+
+#Cria uma nave. O construtor será chamado automaticamente.
+player = Player()
+
+#Cria um grupo de sprite e adiciona a nave.
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
 
 # Comando para evitar travamentos.
 try:
@@ -54,6 +82,12 @@ try:
             if event.type == pygame.QUIT:
                 running = False
     
+        screen.fill(BLACK)
+        screen.blit(background, background_rect)
+        all_sprites.draw(screen)
+        
+        #Depois de desnhar tudo, inverte o display
+        pygame.display.flip()
         # A cada loop, redesenha o fundo e os sprites
         screen.fill(BLACK)
         screen.blit(background, background_rect)
